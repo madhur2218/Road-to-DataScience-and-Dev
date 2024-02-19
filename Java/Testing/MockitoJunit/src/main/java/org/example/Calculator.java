6109b0251a865951713b783cd87903f5e0f0abea
Madhur2218 <madhurnagrath@gmail.com>
2024-02-19 19:09:30 +0530

    Stacks,List, Array, Array2D

---
+++ Java/Testing/MockitoJunit/src/main/java/org/example/Calculator.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Calculator {
+    public int division(int dividend, int divisor)
+        {
+            return dividend/divisor;
+        }
+
+        public int subtraction (int a , int b){
+        return a-b;
+        }
+
+}
+
+
+//Some imp annotations:
+//@Test
+//@Disable
+//@DisplayName
+//@BeforeAll
+//@AfterAll
+//@BeforeeACH
+//@aFTEReACH
