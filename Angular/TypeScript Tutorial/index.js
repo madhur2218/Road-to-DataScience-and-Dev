18d323a1590542c07d75ffec83b58452d3640787
Madhur Nagrath <madhurnagrath@gmail.com>
2024-03-29 14:14:37 +0530

    learning js

---
+++ Angular/TypeScript Tutorial/index.js
@@ -0,0 +1,34 @@
+console.log("hello js")
+let name  = 'madhur';
+console.log(name);
+
+let firstName = 'madhur';
+let age = 234;
+let isApproved = false;
+let lastName = undefined
+let e = null;
+
+
+
+let n = 'mosh'
+let a = 13
+
+let person = {
+    name : 'mosh',
+    age : 12
+
+}
+
+
+console.log(person.age)
+console.log(person)
+let l = 'ashu'
+person [l] = 'sahu';
+console.log(person.name)
+console.log(person)
+
+
+
+let arr = ['red', 'blue']
+console.log(arr)
+console.log(arr[1])
