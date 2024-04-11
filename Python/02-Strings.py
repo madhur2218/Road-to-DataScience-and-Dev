eb2b65b6379a733f939a19c2b177f20b0b4b3321
Maddy2218 <madhurnagrath@gmail.com>
2024-04-11 22:05:59 +0530

    string

--- Python/02-Strings.py
+++ Python/02-Strings.py
@@ -7,8 +7,16 @@ len(message)
 print(message[10])
 print(message[1])
 print(message[11])
+
+#Slicing 
 print(message[0:5:])
-
 print(message[:5])
-
 print(message[6:])
+
+#Some inbuilt methods in strings
+name = "madhurrrr"
+
+print(name.lower())
+print(name.upper())
+print(name.count('m'))
+print(name.find("rrr"))
