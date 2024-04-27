c6662a70374ced92721c13ada7bf2496e8dab010
Maddy2218 <madhurnagrath@gmail.com>
2024-04-27 19:47:16 +0530

    Learning Junit

--- Java/Tim Bachuka/UnitTesting/src/test/java/CalculatorServiceTest.java
+++ Java/Tim Bachuka/UnitTesting/src/test/java/CalculatorServiceTest.java
@@ -10,8 +10,23 @@ public class CalculatorServiceTest {
         int result = CalculatorService.add(2,3);
         //actual result
         //expected result
-        int expected = 50;
+        int expected = 5;
         Assertions.assertEquals(expected,result);
+    }
+
+    @Test
+    public void multTest(){
+        int result = CalculatorService.mult(2,4);
+        int expected = 8;
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void divTest(){
+        double result = CalculatorService.div(2.0,2.0);
+        int expected =1 ;
+        Assertions.assertEquals(expected, result);
+
     }
 
 }
