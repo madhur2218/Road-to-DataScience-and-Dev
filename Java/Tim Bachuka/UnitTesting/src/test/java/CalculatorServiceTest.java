2ff8482074ebb503decd31aa353f47407bf8a1f5
Maddy2218 <madhurnagrath@gmail.com>
2024-04-27 20:11:57 +0530

    Learning Junit

--- Java/Tim Bachuka/UnitTesting/src/test/java/CalculatorServiceTest.java
+++ Java/Tim Bachuka/UnitTesting/src/test/java/CalculatorServiceTest.java
@@ -1,28 +1,38 @@
 import org.example.CalculatorService;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.Date;
 
 public class CalculatorServiceTest {
 
+    int counter = 1;
 
-    @BeforeAll
+
+    @BeforeAll  //before all test cases
     public static void init(){
         System.out.println("run all test cases");
         System.out.println("started all tests" + new Date());
     }
 
+    @BeforeEach
+    public void beforeEach(){
+        System.out.println("Runs before each test cases");
+    }
+
 //test two methods of sum
     @Test
     public void addTest(){
+
+        for(int i=0; i<=20;i++){
+            counter = counter+1;
+        }
+
         int result = CalculatorService.add(2,3);
         //actual result
         //expected result
         int expected = 5;
         Assertions.assertEquals(expected,result);
+        System.out.println("counter after first test case is  "+ counter);
     }
 
     @Test
@@ -40,12 +50,18 @@ public class CalculatorServiceTest {
 
     }
 
-    @AfterAll
+    @AfterAll  //after all test cases
     public static void cleanup(){
         System.out.println("All test cases end");
         System.out.println("End test cases " + new Date());
 
     }
+
+    //@Test
+    //@BeforeAll
+    //@BeforeEach
+    //@AfterAll
+    //@AfterEach
 
 }
 
