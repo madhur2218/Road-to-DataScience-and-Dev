bac5f3590b735b4e21c51fb6089ada10b634118a
Maddy2218 <madhurnagrath@gmail.com>
2024-05-01 11:36:08 +0530

    Java Learning

---
+++ Java/Tim Bachuka/ControlFlow/ControlFlow/src/CodingExercise.java
@@ -0,0 +1,21 @@
+public class CodingExercise {
+    public static void main(String[] args) {
+        checkNumber(12);
+        checkNumber(0);
+        checkNumber(-10);
+
+    }
+
+    public static void checkNumber(int num){
+//        int number= num;
+        if(num>0){
+            System.out.println("positive");
+        } else if (num<0) {
+            System.out.println("negative");
+        }
+        else
+            System.out.println("zero");
+
+//        return number;
+    }
+}
--- Java/Tim Bachuka/ControlFlow/ControlFlow/src/Method_Practice.java
+++ Java/Tim Bachuka/ControlFlow/ControlFlow/src/Method_Practice.java
@@ -2,6 +2,9 @@ public class Method_Practice {
     public static void main(String[] args) {
         score();
         System.out.println(score1(20,20));
+        System.out.println(score1(40,21));
+        System.out.println(isAge(19));
+//        System.out.println(doesSomething(12));
     }
 
     public static void score(){
@@ -15,5 +18,21 @@ public class Method_Practice {
         return a+b;
     }
 
+    //While declaring a void method which does not return anything, we can have a sout statement and all the arguments declared in that method
+    //While declaring a int method which return a integer value can acn pass the parameters in our mthod and in main method we can pass arguments
+
+    public static boolean isAge(int age){
+        boolean result= false;
+        if(age<21){
+            System.out.println("can drink");
+            result = true;
+        }
+        return result;
+    }
+
+    public static void doesSomething(int something){
+        if(something>21)
+            return;
+    }
 
 }
