e4681da0b8bf262f6621e61dbe6774979fe1b4b2
Maddy2218 <madhurnagrath@gmail.com>
2024-05-11 16:58:05 +0530

    java learning

--- Java/Tim Bachuka/cwh_40_AccessModifiers/src/Main.java
+++ Java/Tim Bachuka/cwh_40_AccessModifiers/src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Employee e1= new Employee();
+
+        e1.setName("madhur");
+        System.out.println(e1.getName());
+
+
+
+
     }
 
     
