95bc792a28906a1658219bfe2ffc3e5dcfa20d6a
Maddy2218 <madhurnagrath@gmail.com>
2024-06-13 23:20:25 +0530

    knime

---
+++ Java/Tim Bachuka/DebuggerDemo/src/BankAccount.java
@@ -0,0 +1,15 @@
+public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private double balance;
+
+    public BankAccount(String firstName, String lastName, double balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+
+    
+
+
+}
