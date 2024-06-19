6ead7284e8a929981b0b9d126a4722c3962dc81f
Maddy2218 <madhurnagrath@gmail.com>
2024-06-19 12:55:13 +0530

    java threads

--- Java/Tim Bachuka/CollectionFramework/.idea/sonarlint/securityhotspotstore/index.pb
+++ Java/Tim Bachuka/CollectionFramework/.idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,5 @@
 src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
 B
 src/ArrayList.java,7\a\7a301c81a4a6602e5fb0da1827c26e03b5b0a7d1
+@
+src/Student.java,a\9\a955bfc1fa56842ee29c812417553255d1f41b0e
--- Java/Tim Bachuka/CollectionFramework/src/Main.java
+++ Java/Tim Bachuka/CollectionFramework/src/Main.java
@@ -1,3 +1,5 @@
+import com.sun.source.tree.Tree;
+
 import java.util.*;
 
 public class Main {
@@ -113,6 +115,138 @@ public class Main {
         pq_maxHeap.offer(213);
         pq_maxHeap.offer(213);
         System.out.println(pq_maxHeap);
+
+
+        //HashSet
+
+        HashSet<Integer> h1 = new HashSet<>();
+        h1.add(12);
+        h1.add(23);
+        h1.add(34);
+        h1.add(54);
+        h1.add(54);
+
+        System.out.println("Values is net is " + h1);
+
+        h1.remove(54);
+        System.out.println(h1);
+
+        System.out.println(h1.contains(100));
+
+        System.out.println(h1.isEmpty());
+        System.out.println(h1.size());
+
+        //LinkedHashSet - Insertion order is preserved and duplicates are allowed
+
+        Set h2 = new LinkedHashSet();
+        h2.add(23);
+        h2.add(43);
+        h2.add(32);
+
+        System.out.println(h2);
+
+        //TreeSet - stores values in sorted order
+        Set h3 = new TreeSet(Comparator.reverseOrder());
+        h3.add(43);
+        h3.add(12);
+        h3.add(54);
+
+        System.out.println(h3);
+
+
+        Set<Student> class1 = new HashSet<>();
+
+        class1.add(new Student(12,"MADHUR"));
+        class1.add(new Student(12,"MADHUR"));
+        System.out.println(class1);
+
+
+        //Maps - used to store key-value pair
+        //Keys are unique and values are not unique
+
+        Map<String,Integer> m1 = new HashMap<>();
+        m1.put("one",1);
+        m1.put("two",2);
+        m1.put("three",3);
+        m1.put("four",4);
+        m1.put("five",4);
+        m1.put("four",42);  //value gets overided
+
+        if(m1.containsKey("two  ")){
+            m1.put("two", 22);
+        }
+
+        //Above if condition can be written as -
+        m1.putIfAbsent("two",22);  // in this case value would not get overrided
+        System.out.println(m1);
+
+        for(Map.Entry<String,Integer> e : m1.entrySet()){
+            System.out.println("Keys " + e.getKey() + " Values "+ e.getValue());
+        }
+
+        for(String s : m1.keySet()){
+            System.out.println(s);
+        }
+
+        for(Integer i : m1.values()){
+            System.out.println(i);
+        }
+
+
+        //TreeMap  - stores data key-value pair in alphabetical order of keys
+
+        Map m2 = new TreeMap();
+        m2.put(2,"two");
+        m2.put(3,"three");
+        m2.put(1,"one");
+        m2.put(0,"zero");
+
+        System.out.println(m2);
+        m2.remove(2);
+        System.out.println(m2);
+
+       //ArraysClass
+
+        Integer a[] = {2,32,11,54,65,34,21,45,65};  //Sorting of array
+        Arrays.sort(a);
+
+        for(Integer i : a){
+            System.out.println(i);
+        }
+
+
+        //Binary Search
+        int []numbers = {1,2,3,4,5,6};
+        int index = Arrays.binarySearch(numbers, 3);
+        System.out.println("Index is " + index);
+
+        //Collections Interface
+
+        List l1 = new ArrayList();
+        l1.add(23);
+        l1.add(43);
+        l1.add(1);
+        l1.add(2);
+        l1.add(100);
+
+        System.out.println(Collections.max(l1));
+        System.out.println(Collections.min(l1));
+        System.out.println(Collections.frequency(l1,23));
+        Collections.sort(l1,Comparator.reverseOrder());
+
+
+
+        ArrayList class2 = new ArrayList();
+        class2.add(new Student(10,"MADHUR"));
+        class2.add(new Student(52,"malhar"));
+        class2.add(new Student(32,"ishant"));
+        class2.add(new Student(12,"naman"));
+
+        System.out.println(class2);
+
+
+
+
 
 
 
