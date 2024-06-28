540e6b26227acafc6f0b50c57ee32dbe6c7b970f
Maddy2218 <madhurnagrath@gmail.com>
2024-06-28 21:53:49 +0530

    java

---
+++ Java/Chad Darby/02-spring-boot-spring-core/03-setter-injection/src/main/java/com/constructorInjection/springCoreDemo/CricketCoach.java
@@ -0,0 +1,12 @@
+package com.constructorInjection.springCoreDemo;
+
+import org.springframework.stereotype.Component;
+@Component
+public class CricketCoach implements Coach {
+
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes !" ;
+    }
+}
