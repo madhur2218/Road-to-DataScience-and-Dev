284fc46e33339871295152608c85b4910436d94d
Maddy2218 <madhurnagrath@gmail.com>
2024-07-20 23:16:49 +0530

    java basics

---
+++ FULL STACK JAVA DEV/Section1/Hello-World/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+
+    //https://www.udemy.com/course/full-stack-java-developer-java/learn/lecture/41297560#overview
+    //go through above article
+}
