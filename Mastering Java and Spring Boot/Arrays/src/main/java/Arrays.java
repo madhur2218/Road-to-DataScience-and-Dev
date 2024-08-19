7f81ca60856f5541c2c22ef6f0d66a4aae1fa1d4
Maddy2218 <madhurnagrath@gmail.com>
2024-08-19 23:14:42 +0530

    Arrays

---
+++ Mastering Java and Spring Boot/Arrays/src/main/java/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int [] a = new int[10];
+        a[1]= 12;
+
+        //Using array initializer
+        int [] arr1 = new int [] {1,2,3,3,4,5};
+     }
+}
