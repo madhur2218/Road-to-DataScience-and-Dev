8729499a8c4babbdf6f56c0cb6a6583769e4a82d
madhur2218 <madhurnagrath@gmail.com>
2024-10-23 12:11:39 +0530

    todo app using flask

--- Flask/CodeWithHarry/app.py
+++ Flask/CodeWithHarry/app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////test.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db= SQLAlchemy(app)
+
+
+class TODO(db.Model):
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
+
+@app.route("/products")
+def products():
+    return "this is products page"
+
+@app.route("/cricketCoach")
+def cricketCoach():
+    return "this is cricketCoach"
+
+@app.route("/renderTemplate")
+def renderTemplate():
+    return render_template('index.html')
+
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True, port = 8000)
