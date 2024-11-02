e3b62959b4bde085d24cef275211befc3bebe3bf
madhur2218 <madhurnagrath@gmail.com>
2024-11-02 11:42:50 +0530

    object class in java

---
+++ Core Java Concepts/TheObjectClass/src/PrimarySchoolStudent.java
@@ -0,0 +1,16 @@
+public class PrimarySchoolStudent extends Student{
+
+    private String parentName;
+
+    public PrimarySchoolStudent(String name, int age, String parentName) {
+        super(name, age);
+        this.parentName=parentName;
+    }
+
+    @Override
+    public String toString() {
+        return "PrimarySchoolStudent{" +
+                "parentName='" + parentName + '\'' +
+                "} " + super.toString();
+    }
+}
