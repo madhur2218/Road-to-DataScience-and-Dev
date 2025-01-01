0d19a4673f3257d18096adb318935a6d04b8e1cf
madhur2218 <madhurnagrath@gmail.com>
2025-01-01 18:18:41 +0530

    dsa-basics

---
+++ DSA_2025/pepcoding/LEVEL1_FOUNDATION/lec1_prinitingOutput.java
@@ -0,0 +1,15 @@
+public class lec1_prinitingOutput {
+    public static void main(String[] args) {
+        System.out.print("Hello world\n");
+        System.out.println("hello world");
+        System.out.print("hello world");
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("*****");
+        System.out.println("******");
+
+        System.out.print("*\n**\n***\n****\n*****");
+    }
+}
---
+++ DSA_2025/pepcoding/LEVEL1_FOUNDATION/lec2_printingZpattern.java
@@ -0,0 +1,9 @@
+public class lec2_printingZpattern {
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
+}
