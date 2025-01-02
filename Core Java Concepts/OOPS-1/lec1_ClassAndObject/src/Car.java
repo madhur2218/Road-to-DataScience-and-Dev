8e74adb5f424594c56e5cd744017c8b3e4639608
madhur2218 <madhurnagrath@gmail.com>
2025-01-02 23:35:59 +0530

    classAndObjects

--- Core Java Concepts/OOPS-1/lec1_ClassAndObject/src/Car.java
+++ Core Java Concepts/OOPS-1/lec1_ClassAndObject/src/Car.java
@@ -1,11 +1,26 @@
 public class Car {
-    private String make;
-    private String model;
-    private String color;
-    private int doors;
-    private boolean convertible;
+    private String make = "Tesla";
+    private String model = "Model x";
+    private String color= "black";
+    private int doors = 4;
+    private boolean convertible = true;
 
-
-
-
+    public void describeCar(){
+        System.out.println("Car make is "+ make+
+                " model "+ model +
+                " color "+ color+
+                " doors "+ doors +
+                " and " +
+                (convertible ? "Convertable": " "));
+    }
+    @Override
+    public String toString() {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", doors=" + doors +
+                ", convertible=" + convertible +
+                '}';
+    }
 }
--- Core Java Concepts/OOPS-1/lec1_ClassAndObject/src/Main.java
+++ Core Java Concepts/OOPS-1/lec1_ClassAndObject/src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Car c1 = new Car();
+        c1.describeCar();
     }
 }
