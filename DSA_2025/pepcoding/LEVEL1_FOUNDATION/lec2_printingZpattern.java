6fd9ee97e83b625eb3f784f804b15c3d14fb90f4
Maddy2218 <madhurnagrath@gmail.com>
2025-04-09 09:20:45 +0530

    Deleting unnecessary files
    Deleting unnecessary files

--- DSA_2025/pepcoding/LEVEL1_FOUNDATION/lec2_printingZpattern.java
+++
@@ -1,9 +0,0 @@
-public class lec2_printingZpattern {
-    public static void main(String[] args) {
-        System.out.println("*****");
-        System.out.println("   *");
-        System.out.println("  *");
-        System.out.println(" *");
-        System.out.println("*****");
-    }
-}
--- DSA_2025/striver-a-z/demo.java
+++
@@ -1,6 +0,0 @@
-class Demo{
-    public static void main(String a[]){
-
-        System.out.println("21");
-    }
-}
