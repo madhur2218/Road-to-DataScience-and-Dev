43420b9dcafc98d20c02171159bb91c57e19fccd
Maddy2218 <madhurnagrath@gmail.com>
2025-04-10 23:16:36 +0530

    learning

---
+++ DSA_2025/Basic Java/java_basic_syntax/src/Main.java
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Primitive Data types
+
+        //Integers
+        int num1 = 10;
+        System.out.println(num1);
+
+        //Float
+        float num2 = 40f;
+        System.out.println(num2);
+
+        //Double
+        double num3 = 30.45d;
+        System.out.println(num3);
+
+        //Char
+        char char1= 'c';
+        System.out.println(char1);
+
+        //Boolean
+        boolean isTrue = true;
+        System.out.println(isTrue);
+
+        //String
+        String str1 = "Madhur";
+        System.out.println(str1);
+
+    }
+}
