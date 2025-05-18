69fbfa9d51f3571d7434f69464ac91683c56c752
Maddy2218 <madhurnagrath@gmail.com>
2025-05-18 23:09:17 +0530

    java strings

--- Java/Core-Java-TimBuchaka/strings/src/Main.java
+++ Java/Core-Java-TimBuchaka/strings/src/Main.java
@@ -2,34 +2,59 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        printInformation("MADHUR");
         printInformation("");
-        printInformation("  ");
+        String helloWorld = "Hello World";
+        System.out.println("Index of o is: " +helloWorld.indexOf('o'));
 
+        System.out.println("Index of world : " +helloWorld.indexOf("World"));
+        System.out.println("Index of l: "+ helloWorld.indexOf('l'));
+        System.out.println("Index of l: "+ helloWorld.lastIndexOf('l'));
+        System.out.println("Index of l: "+ helloWorld.lastIndexOf('l',3));
+
+        String nameUpperCase = "MADHUR";
+        String namelowerCase = "madhur";
+
+        if(nameUpperCase.equals(namelowerCase)){
+            System.out.println("String match");
+        }
+        if(namelowerCase.equalsIgnoreCase(nameUpperCase)){
+            System.out.println("String match including case");
+        }
+
+        if(namelowerCase.startsWith("mad")){
+            System.out.println("String starts with mad");
+        }
+
+        if(namelowerCase.endsWith("r")){
+            System.out.println("Ends with r");
+        }
+
+        if(namelowerCase.contains("m")){
+            System.out.println("String contains m");
+        }
+
+        if(namelowerCase.contentEquals("m")){   //contentEquals is more specific to string builder and not to normla string
+            System.out.println("String contains m");
+        }
 
     }
 
-    public static void printInformation(String string){
-        int length = string.length();
-        System.out.printf("length of string is %d %n", length);
-        if(string.isEmpty()){
-            System.out.println("String is empty and length is "+ string.length());
+    public static void printInformation(String s){
+        int length = s.length();
+        System.out.println("Length is: "+ s.length());
+
+        if(s.isEmpty()){
+            System.out.println("String is empty");
             return;
         }
-        else if (string.isBlank()) {
+
+        if(s.isBlank()){
             System.out.println("String is blank");
+            return;
         }
-
-        //System.out.println("length of string is %d"+ length);
-        System.out.printf("First character is %c %n", string.charAt(0));
-        System.out.println("Last character of string is "+ string.charAt(length-1));
-
-//        for(int i=0; i<=string.length();i++){
-//            System.out.println(string.charAt(i));
-//        }
-
+        System.out.println("First character is: "+ s.charAt(0));
+        System.out.println("Last character is: "+ s.charAt(length-1));
 
 
     }
-
 }
