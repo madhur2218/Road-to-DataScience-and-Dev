38ad87aa5e308a793dfcf53d98a4cfd2bb378485
Maddy2218 <57710530+madhur2218@users.noreply.github.com>
2025-11-24 10:46:41 +0100

    Learning springboot

--- Chad Darby/dev-spring-boot/mycoolapp/src/main/java/com/luv2code/springboot/demo/rest/Controller.java
+++ Chad Darby/dev-spring-boot/mycoolapp/src/main/java/com/luv2code/springboot/demo/rest/Controller.java
@@ -8,20 +8,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     //expose "/" that return "Hello World"
-  @GetMapping("/")
-    public String hello() {
-      return "Hello World";
-  }
-
-  @GetMapping("/cooking")
-      public String cooking(){
-          return "I love cooking!";
+      @GetMapping("/")
+        public String hello() {
+          return "Hello World";
       }
+
+      @GetMapping("/cooking")
+          public String cooking(){
+              return "I love cooking!";
+          }
 
       @GetMapping("/health")
         public String health(){
             return "I love health!";
         }
+
+        @GetMapping("eating")
+            public String eating(){
+                return "I love eating!";
+            }
+        @GetMapping("travelling")
+            public String travelling(){
+                return "I love travelling!";
+        }
+
   }
 
 
